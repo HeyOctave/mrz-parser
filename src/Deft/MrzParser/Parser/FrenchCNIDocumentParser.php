9e86f3e9a3bcbeb8a2d1471796423b9b6db0f800
Arthur de Moulins <arthurdemoulins@gmail.com>
2019-11-28 11:46:28 +0100

    add french ID parser

---
+++ src/Deft/MrzParser/Parser/FrenchCNIDocumentParser.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Deft\MrzParser\Parser;
+
+use Deft\MrzParser\Document\TravelDocument;
+use Deft\MrzParser\Document\TravelDocumentInterface;
+use Deft\MrzParser\Document\TravelDocumentType;
+use Deft\MrzParser\Exception\ParseException;
+
+/**
+ * Parser of french CNI documents.
+ * @see https://fr.wikipedia.org/wiki/Carte_nationale_d%27identit%C3%A9_en_France
+ *
+ * @package Deft\MrzParser\Parser
+ */
+class FrenchCNIDocumentParser extends AbstractParser
+{
+    /**
+     * Extracts all the information from a MRZ string and returns a populated instance of TravelDocumentInterface
+     *
+     * @param $string
+     * @return TravelDocumentInterface
+     * @throws ParseException
+     */
+    public function parse($string)
+    {
+        if ('ID' !== $this->getToken($string, 1, 2)) {
+            throw new ParseException("First characters are not 'ID'");
+        }
+
+        $secondLine = 36;
+
+        $fields = [
+            'type' => TravelDocumentType::ID_CARD,
+            'primaryIdentifier' => $this->getToken($string, 6, 30),
+            'documentNumber' => $this->getToken($string, $secondLine + 1, $secondLine + 12),
+            'secondaryIdentifier' => $this->getNames($string, $secondLine + 14, $secondLine + 27)[0],
+            'dateOfBirth' => $this->getDateToken($string, $secondLine + 28),
+            'sex' => $this->getToken($string, $secondLine + 35),
+            'nationality' => $this->getToken($string, 3, 5)
+        ];
+
+        return new TravelDocument($fields);
+    }
+}
